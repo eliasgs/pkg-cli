50571d46483977cc083533b076ff0e4ab7a82c2b
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-21 22:54:58 +0100

    Make it possible to reference field properties

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var pkg = require(process.env.PWD + '/package.json');
 
-function get(field) {
-  return pkg[field] || '';
+function get(arg) {
+  // Match all fields and array indexes of arg.
+  var props = arg.split(/(?:(\w+)\.)|(?:(\w+)\[(\w+)\]\.?)|(?:(\w+)$)/g)
+                 .filter(function (el) {return el}); // remove empty elements
+  var tmp = pkg;
+  props.forEach(function (prop) {
+    tmp = tmp && tmp[prop];  
+  });
+  return tmp;
 }
 
 exports.get = get;
