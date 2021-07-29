d890ef3e236ba89f8c5e9a82a48121a8b2f68ab8
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-07-29 10:20:07 -0400

    add eror handling
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { readFileSync } from 'fs';
 const pkg_path = process.env.PWD + '/package.json';
-function get<T = any>(arg: string) {
+function get(arg: string): string {
   // Match all fields and array indexes of arg.
   const props = arg
     .split(/(?:(\w+)\.)|(?:(\w+)\[(\w+)\]\.?)|(?:(\w+)$)/g)
     .filter(el => !!el); // remove empty elements
-  let tmp = JSON.parse(readFileSync(pkg_path, { encoding: 'utf8' }));
-  props.forEach(prop => {
-    tmp = tmp && tmp[prop];
-  });
-  return tmp as T;
+  try {
+    let tmp = JSON.parse(readFileSync(pkg_path, { encoding: 'utf8' }));
+    props.forEach(prop => {
+      tmp = tmp && tmp[prop];
+    });
+    return String(tmp);
+  } catch (error: any) {
+    return error && typeof error.message === 'string'
+      ? String(error.message)
+      : 'ERROR: operation failed for an unknow reason';
+  }
 }
 
 export { get };
